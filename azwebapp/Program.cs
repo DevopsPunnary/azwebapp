3534b24a7d01005b15ccd176802cbe2c0f8c37bf
azdevopstest1 <azdevopstest1@outlook.com>
2024-09-21 17:38:12 +0530

    Update Program.cs

--- azwebapp/Program.cs
+++ azwebapp/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Az DevopsApp11115!");
+app.MapGet("/", () => "Az DevopsApp11117!");
 
 app.Run();
