6e9cc560ef407c27ce8318978dae2751f4320403
Punnary12 <52101396+Punnary12@users.noreply.github.com>
2024-09-22 07:16:15 +0530

    Update Program.cs

--- azwebapp/Program.cs
+++ azwebapp/Program.cs
@@ -1,6 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Az DevopsApp11117!");
+app.MapGet("/", () => "Az DevopsApp11118!");
 
 app.Run();
